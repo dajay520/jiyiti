2a8708b6f5e0a9a0b21f5d5ee45a43ababf2062b
tian junjie <dajay520@gmail.com>
2012-07-29 18:23:52 +0800

    ss

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,7 @@ Reduce::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'loseweight#index'
+  root :to => 'loseweights#index'
 
   # See how all your routes lay out with "rake routes"
 
