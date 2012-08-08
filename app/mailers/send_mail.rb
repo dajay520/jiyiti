ebf384cb289c03d31296aa6f7b144c4856aa1037
tian junjie <dajay520@gmail.com>
2012-08-08 23:10:16 +0800

    mail v1

--- app/mailers/send_mail.rb
+++ app/mailers/send_mail.rb
@@ -9,9 +9,9 @@ class SendMail < ActionMailer::Base
   #
   def confirm
     if Loseweight.where(:update_date => '2012-08-04').size < 1
-      mail to: "dajay520@qq.com;dajay520@gmail.com",:subject => "日报延迟通知"
+      mail to: ["dajay520@qq.com","dajay520@gmail.com"],:subject => "日报延迟通知"
     else
-      mail to: "dajay520@qq.com;dajay520@gmail.com",:subject => "日报没有延迟通知"
+      mail to: ["dajay520@qq.com","dajay520@gmail.com"],:subject => "日报没有延迟通知"
     end
   end
 end
