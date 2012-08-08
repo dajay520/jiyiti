7be62a5f34bb9c7131ae131340eebba8bef1afcb
tian junjie <dajay520@gmail.com>
2012-08-08 23:45:21 +0800

    mail v2

--- config/initializers/sendmail_timer.rb
+++ config/initializers/sendmail_timer.rb
@@ -2,7 +2,7 @@ puts 'init timer'
 Thread.start do
   puts 'thread run'
   loop do
-    now_time = DateTime.now
+    now_time = Time.now + 8.hour
     nd = now_time + 1.day
     nd = Time.parse(nd.strftime("%y-%m-%d"))
     sleep nd.to_f-now_time.to_f
